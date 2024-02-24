26ea194f2e903d9690304eafe6bc148c0b34d8ba
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2024-02-24 16:36:18 -0300

    Adicionando o app-footer no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizzComponent } from './components/quizz/quizz.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './template/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuizzComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
